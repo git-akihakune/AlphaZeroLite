7cbebc7450958a01f32f1cdf6deb4f700569a22f
Aki Hakune <akihakune@protonmail.ch>
2023-02-05 08:27:02 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from setuptools import setup
+from src import zerolite
+
+setup(
+    name=zerolite.__name__,
+    version=zerolite.__version__,
+    description=zerolite.__description__,
+    url=zerolite.__url__,
+    author=zerolite.__author__,
+    license=zerolite.__license__,
+    packages=['zerolite'],
+    install_requires=[
+        'torch>=1.13.1',
+        'python-chess'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Education :: Testing',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ]
+)
--- src/zerolite/__init__.py
+++ src/zerolite/__init__.py
@@ -6,4 +6,10 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
+# Package constants
+__name__ = 'zerolite'
 __version__ = '0.0.1'
+__description__ = "Chess neural network based on Alpha Zero's algorithm"
+__url__ = 'https://github.com/git-akihakune/AlphaZeroLite'
+__author__ = 'Aki Hakune'
+__license__ = 'MIT'
