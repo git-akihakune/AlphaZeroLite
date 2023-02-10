c2e6a6a6a170b470200415c1df7673f1f575005e
Aki Hakune <akihakune@protonmail.ch>
2023-02-10 08:38:08 +0000

    Add model load option in arguments

--- src/zerolite/arguments.py
+++ src/zerolite/arguments.py
@@ -5,13 +5,14 @@ ZeroLite - Alpha Zero chess in a library.
 
 Usage:
     zerolite train [--data <path>] [-out <path>]
-    zerolite play
+    zerolite play [--model <path>]
     zerolite -h | --help
     zerolite --version
 
 Options:
     -d --data PATH  Path to dataset.
     -o --out PATH   Model save location.
+    -m --model PATH Path to saved model.
     -h --help       Show this screen.
     -v --version    Show version.
 """
